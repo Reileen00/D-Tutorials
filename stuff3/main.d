39970c8091132c280447e195e8e43402400dd80b
Baisnabi Rout <100047799+Reileen00@users.noreply.github.com>
2025-05-27 18:56:49 +0530

    Create main.d

---
+++ stuff3/main.d
@@ -0,0 +1,18 @@
+import std.stdio;
+import std.string;
+
+void main(){
+  int[10] age=0;
+  char[21] oneName;
+  char[10][21]manynames;
+
+  string onename_string=readln();
+  
+  string[10] myname_string;
+
+  int[] dynamicarrays;
+  dynamicarrays.length=3;
+  dynamicarrays ~=34;
+
+  string name=strip(readln());
+}
