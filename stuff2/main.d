e38de9a780774fe0e8bc8f9472ee4281428a804b
Baisnabi Rout <100047799+Reileen00@users.noreply.github.com>
2025-05-27 18:51:14 +0530

    Create main.d

---
+++ stuff2/main.d
@@ -0,0 +1,10 @@
+import std.stdio: writeln,write,readf;
+void main(){
+  float a=1;
+  float b=2;
+  char operations;
+  read("%s %s %s",a,b,operations);
+  
+  if(operation=='+')return a+b;
+  //......
+}
